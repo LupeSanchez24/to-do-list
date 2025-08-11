5133f6070ed283daa036d008968bcf913b1c17d5
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2025-08-11 16:30:21 -0700

    Added toggle on visbility with rotating arrow icon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import "../Main/Main.css";
 import getDownIcon from "../../assets/get-down-icon.svg";
 function Main() {
   const [currentDate, setCurrentDate] = useState(getDate());
+  const [isVisible, setIsVisible] = useState(true);
 
   function getDate() {
     const today = new Date();
@@ -10,26 +11,79 @@ function Main() {
     return today.toLocaleDateString("en-US", options);
   }
 
+  function toggleVisibility() {
+    setIsVisible((prev) => !prev);
+  }
+
   return (
     <main className="toDo">
       {" "}
       <h2 className="toDo__date"> Today is {currentDate}</h2>
       <p className="toDo__title"> Your Tasks for the week:</p>
-      <div className="toDo__container">
+      <div className="toDo__header">
         <p className="toDo__task-date">Today:</p>
-        <img className="toDo__icon" src={getDownIcon} alt="get down icon" />
-        <input type="checkbox" className="toDo__checkbox" id="exerciseToday" />
-        <label className="toDo__activity" htmlFor="exerciseToday">
-          Exercise
-        </label>
-        <input type="checkbox" className="toDo__checkbox" id="laundryToday" />
-        <label className="toDo__activity" htmlFor="laundryToday">
-          Laundry
-        </label>
-        <input type="checkbox" className="toDo__checkbox" id="readToday" />
-        <label className="toDo__activity" htmlFor="readToday">
-          Read
-        </label>
+        <div className="toDo__image">
+          <img
+            className={`toDo__icon ${isVisible ? "toDo__icon--rotated" : ""}`}
+            src={getDownIcon}
+            alt="toggle to-do list"
+            onClick={toggleVisibility}
+          />
+        </div>
+      </div>
+      {isVisible && (
+        <div className="toDo__container">
+          <input
+            type="checkbox"
+            className="toDo__checkbox"
+            id="exerciseToday"
+          />
+          <label className="toDo__activity" htmlFor="exerciseToday">
+            Exercise
+          </label>
+          <input type="checkbox" className="toDo__checkbox" id="laundryToday" />
+          <label className="toDo__activity" htmlFor="laundryToday">
+            Laundry
+          </label>
+          <input type="checkbox" className="toDo__checkbox" id="readToday" />
+          <label className="toDo__activity" htmlFor="readToday">
+            Read
+          </label>
+        </div>
+      )}
+      {/*Before*/}
+      {/*<div className="toDo__container">
+        <p className="toDo__task-date">Today:</p>
+        <img
+          className="toDo__icon"
+          src={getDownIcon}
+          alt="toggle to-do list"
+          onClick={toggleVisibility}
+        />
+        {isVisible && (
+          <>
+            <input
+              type="checkbox"
+              className="toDo__checkbox"
+              id="exerciseToday"
+            />
+            <label className="toDo__activity" htmlFor="exerciseToday">
+              Exercise
+            </label>
+            <input
+              type="checkbox"
+              className="toDo__checkbox"
+              id="laundryToday"
+            />
+            <label className="toDo__activity" htmlFor="laundryToday">
+              Laundry
+            </label>
+            <input type="checkbox" className="toDo__checkbox" id="readToday" />
+            <label className="toDo__activity" htmlFor="readToday">
+              Read
+            </label>
+          </>
+        )}
       </div>
       <div className="toDo__container">
         <p className="toDo__task-date">Tomorrow:</p>
@@ -78,7 +132,7 @@ function Main() {
         <label className="toDo__activity" htmlFor="readThisWeek">
           Read
         </label>
-      </div>
+        </div>*/}
     </main>
   );
 }
